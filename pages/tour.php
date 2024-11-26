772b72f1e4efbb5ed3872b53d18cb799b1fd85d0
plus-agc <yamauchi-t@plus-agc.com>
2024-11-26 14:11:12 +0900

    modified

--- pages/tour.php
+++ pages/tour.php
@@ -6,20 +6,9 @@ get_header();
 ?>
 
 <main>
-    <section class="uk-section uk-page-header">
-        <div class="uk-container">
-            <?php
-            include get_template_directory() . '/components/breadcrumb.php';
-            if (function_exists('display_breadcrumb')) {
-                display_breadcrumb();
-            }
-            ?>
-        </div>
-    </section>
-
     <section class="uk-section" id="about_header">
         <div class="uk-container">
-
+            <p>準備中</p>
         </div>
     </section>
 
