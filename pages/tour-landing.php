f62496cc2b6410277ec92e5f1ec703ba5f0730b3
plus-agc <yamauchi-t@plus-agc.com>
2024-12-02 17:33:45 +0900

    modified

---
+++ pages/tour-landing.php
@@ -0,0 +1,197 @@
+<?php
+/*
+Template Name: サテライトオフィスモニターツアーLP
+*/
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- OGP meta tags -->
+    <meta property="og:title" content="TSURUTA LABO - 鶴田町地域活性化支援センター" />
+    <meta property="og:description" content="青森県鶴田町にある地域活性化支援センター。旧水元中央小学校をリノベーションし、貸オフィス、コワーキングスペース、イベントスペース、シェアショップを展開。地域の活性化と持続的発展を支援します。" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://tsurutalabo.com/" />
+    <meta property="og:image" content="https://tsurutalabo.com/wp-content/uploads/2024/11/ogpimage.jpg" />
+    <meta property="og:locale" content="ja_JP" />
+    <meta property="og:site_name" content="TSURUTA LABO" />
+
+    <!-- Twitter Card meta tags -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="TSURUTA LABO - 鶴田町地域活性化支援センター" />
+    <meta name="twitter:description" content="青森県鶴田町にある地域活性化支援センター。旧水元中央小学校をリノベーションし、貸オフィス、コワーキングスペース、イベントスペース、シェアショップを展開。地域の活性化と持続的発展を支援します。" />
+    <meta name="twitter:image" content="https://tsurutalabo.com/wp-content/uploads/2024/11/ogpimage.jpg" />
+
+    <link rel="stylesheet" id="uikit-css" href="https://tsurutalabo.com/wp-content/themes/tsurutalabo/css/uikit.min.css?ver=1.0.0" type="text/css" media="all">
+    <link rel="stylesheet" id="main-css" href="https://tsurutalabo.com/wp-content/themes/tsurutalabo/css/main.css?ver=1.0.0" type="text/css" media="all">
+</head>
+
+<main>
+    <section class="uk-section" id="residence_header">
+        <div class="uk-container">
+            <div class="uk-margin-large-bottom">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/header-img.jpg" alt="">
+            </div>
+            <div class="uk-margin-auto uk-width-fit uk-margin-large-bottom">
+                <div class="uk-width-3-4@m uk-margin-auto">
+                    <p class="uk-text-large">2024年11月、<span style="color:#E95513;" class="uk-text-bold">廃校になった小学校を活用</span>した複合施設「<span style="color:#E95513;" class="uk-text-bold">TSURUTA LABO</span>」がオープン！
+                        コワーキングスペースや貸オフィス等を兼ね備えたTSURUTA LABOに、<span style="color:#E95513;" class="uk-text-bold">サテライトオフィス開設等の関心がある企業・個人の方を対象</span>とした見学・体験ツアーを実施します。
+                        自然豊かで四季のうつろいを感じられる青森県鶴田町をぜひご体感ください。</p>
+                </div>
+            </div>
+            <div class="uk-margin-large-bottom">
+                <div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">実施日程</p>
+                        <p style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1">2025年 2月8日[土]・2月9日[日]<span class="uk-text-default uk-margin-left"><img src="<?php echo get_template_directory_uri(); ?>/images/icon-cal.svg" alt="" width="40" class="uk-margin-small-right">1泊2日 ツアースケジュールはこちら！</span></p>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">定　　員</p>
+                        <div style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1">
+                            <p>5名</p>
+                            <ul class="uk-list uk-text-default">
+                                <li>※1企業・団体から1名まで</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">申込対象</p>
+                        <div style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1 uk-text-default">
+                            <ul class="uk-list">
+                                <li>■ 青森県鶴田町でのサテライトオフィス開設に関心がある企業・個人等</li>
+                                <li>■ ツアー実施後のアンケートに回答できる方</li>
+                                <li>■ 日本国内在住の方</li>
+                            </ul>
+                            <p>※ツアーの様子を、個人が特定されない範囲で当センターのHP・SNS等に公開することがあります</p>
+                        </div>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">参加費</p>
+                        <div style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1">
+                            <p>無料</p>
+                            <ul class="uk-list uk-text-default">
+                                <li>※自宅から東京駅までの往復旅費、一部の飲食代は自己負担となります</li>
+                                <li>※宿泊先は鶴田町「つがる富士見荘」です</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">申込締切</p>
+                        <div style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1">
+                            <p>2025年1月8日［水］</p>
+                            <ul class="uk-list uk-text-default">
+                                <li>※定員に達し次第締め切り</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto">
+                        <p class="uk-background-primary uk-text-white uk-padding-small uk-margin-remove uk-width-fit uk-text-bold">参加登録・申込方法</p>
+                        <div style="background-color:#BAE0CF;" class="uk-padding-small uk-margin-remove uk-text-large uk-text-bold uk-inline uk-width-1-1">
+                            <p><a href="#tour-reserve-form" class="uk-border-solid uk-link-reset">申込専用フォーム</a>からお申し込みください</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-margin-large-bottom">
+                <div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom uk-text-center uk-text-large uk-text-bold">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/main-logo.svg" alt="">
+                        <p>青森県北津軽郡鶴田町大字妙堂崎字杉元75</p>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <ul class="uk-list">
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-image01.jpg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-image02.jpg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-image03.jpg" alt=""></li>
+                        </ul>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <ul class="uk-list">
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-title.svg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-schedule-title.svg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-schedule-date.svg" alt=""></li>
+                        </ul>
+                    </div>
+                    <div class="uk-width-3-4@m uk-margin-auto uk-margin-bottom">
+                        <ul class="uk-list">
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-day1.jpg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-day2.jpg" alt=""></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/main-tour-footer.svg" alt=""></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-margin-large-bottom">
+                <div style="border:solid 1px #000;">
+                    <p style="background-color:#3E3A39;" class="uk-width-1-1 uk-text-large uk-text-bold uk-text-white uk-padding-small">旅行条件書(抜粋)</p>
+                    <ul class="uk-list uk-width-5-6 uk-margin-auto">
+                        <li>
+                            <p style="background-color:#717071;" class="uk-padding-small uk-text-white">1.募集型企画旅行契約</p>
+                            <p style="color:#000;">この旅行は㈱プラスが企画し、フラワー観光㈱が旅券発券等手配をする旅行です。旅行契約の内容・条件は、募集広告、チラシ、本旅行出発前にお渡しする確定書面（最終旅行日程表）及びフラワー観光㈱旅行業約款募集型企画旅行契約によります。</p>
+                        </li>
+                        <li>
+                            <p style="background-color:#717071;" class="uk-padding-small uk-text-white">2.旅行中止の場合</p>
+                            <p style="color:#000;">新幹線の運行状況及び天候等によりツアーの中止、またはツアー内容を変更する場合があります。</p>
+                        </li>
+                        <li>
+                            <p style="background-color:#717071;" class="uk-padding-small uk-text-white">3.ツアーに含まれるもの含まれないもの</p>
+                            <p style="color:#000;">（1）東京駅・新青森駅間の新幹線乗車料、ツアーの目的地に移動するためのタクシー利用料、宿泊費、2日目の朝食及び昼食代が含まれます。</p>
+                            <p style="color:#000;">（2）この旅行には国内企画旅行補償制度に加入いたします。</p>
+                            <p style="color:#000;">補償内容抜粋は下記の通りです。詳細についてはお問い合せください。</p>
+                            <table class="uk-table uk-table-divider" style="color:#000;">
+                                <tbody>
+                                    <tr>
+                                        <td>傷害死亡</td>
+                                        <td>1,000万円</td>
+                                    </tr>
+                                    <tr>
+                                        <td>入院日額</td>
+                                        <td>4,000円</td>
+                                    </tr>
+                                    <tr>
+                                        <td>通院日額</td>
+                                        <td>2,500円</td>
+                                    </tr>
+                                    <tr>
+                                        <td>賠償責任</td>
+                                        <td>2,000万円</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <p style="color:#000;">（3）旅行日程に記載のない交通費などの諸費及び1日目の昼食・夕食代など個人的性質の諸費用は含まれません。</p>
+                        </li>
+                        <li>
+                            <p style="background-color:#717071;" class="uk-padding-small uk-text-white">4.お客様からの旅行契約の解除（取り消し料）</p>
+                            <p style="color:#000;">本ツアーは鶴田町が旅行代金の助成を行っているため、参加取消における取消料はございません。</p>
+                            <p style="color:#000;">※キャンセルが確定次第、㈱プラスへすみやかにご連絡ください</p>
+                        </li>
+                        <li>
+                            <p style="background-color:#717071;" class="uk-padding-small uk-text-white">5.個人情報の取扱いについて</p>
+                            <p style="color:#000;">㈱プラス及びフラワー観光㈱では、お客様からご提供いただいた個人情報について、お客様との連絡のためや運送、宿泊機関等の提供するサービスの手配及び受領のために必要な範囲内で利用させていただきます。</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h2 class="uk-heading uk-text-center uk-text-uppercase uk-text-bolder uk-position-relative" id="tour-reserve-form">
+                <span>
+                    <div class="uk-navbar-subtitle">- tour reservation -</div>サテライトオフィスモニターツアーのお申し込み
+                </span>
+            </h2>
+            <div class="uk-margin-auto uk-width-fit uk-margin-large-bottom">
+                <div class="uk-width-3-4@m uk-margin-auto">
+                    <ul class="uk-list">
+                        <li>〇実施日程：2025年2月8日（土）・2月9日（日）</li>
+                        <li>〇申込期限：2025年1月8日（水）</li>
+                        <li>※チラシ・ポスターには「定員に達し次第締め切り」とありますが、申込期限まで申込を受付いたします。</li>
+                        <li class="uk-margin-remove">※申込者多数の場合、抽選となります。</li>
+                        <li class="uk-margin-remove">※2025年1月10日（金）を目途に、メールにて参加決定/不決定のお知らせをいたします。</li>
+                        <li class="uk-margin-remove">※企業・団体所属の方は、企業・団体の名称やご住所、電話番号もご入力ください。</li>
+                    </ul>
+                </div>
+            </div>
+            <?php the_content(); ?>
+        </div>
+    </section>
+</main>
